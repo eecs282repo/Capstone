7a76895d643328d1d3da13422ed159355482addd
Yutong Wang <hybridthesis@gmail.com>
2012-04-11 19:01:12 -0400

    "Trying merges"

--- capstone/app/controllers/Application.java
+++ capstone/app/controllers/Application.java
@@ -8,6 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
+	  // Comment from Yutong
     return ok(index.render("Your new application is ready."));
   }
   
